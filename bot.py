c89162f42b8e20f043ef4debb1751fa077d26a45
Nicstim <56487341+nicstim@users.noreply.github.com>
2020-06-20 01:58:02 +0300

    update template

--- bot.py
+++ bot.py
@@ -4,13 +4,10 @@ import requests
 import sqlite3
 import threading
 import time
-import bs4
+# import bs4
 
-
-# Функция парсера
-def parser():
-    pass
-
+fruits = ["яблоко", "бананы", "груша"]
+vegetables = ["помидоры", "огурцы", "огурец"]
 
 # Создание пользователя
 def create_user(id,city):
@@ -35,6 +32,11 @@ def read_user(id):
     return push
 
 
+# Получение user_id всех пользователей
+def read_all_user():
+    pass
+
+
 # Обновление данных о пользователе
 def update_user(id,push):
     pass
@@ -46,7 +48,15 @@ admin_id = "270943665"
 
 bot = telebot.TeleBot("1197512104:AAFbg4You7T1DHUOr3Ocklz4Z94qXtfHgoY")
 
+# Функция парсера
+def parser():
+    pass
+
+
 print("bot start...")
+
+t_0 = threading.Thread(target = parser, name = "Парсер", args = (id,))
+
 # обработака /start
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -55,13 +65,20 @@ def start(message):
     bot.send_message(message.chat.id, f"""
 Приветствую тебя,{message.from_user.first_name}.
 
-Я фермер Mr. Foodsharing , добро пожаловать на мою ферму.
+Меня зовут Mr. Foodsharing , добро пожаловать на мою ферму.
     """, reply_markup = menu)
+
+    adm = types.InlineKeyboardMarkup(row_width=2)
+    btn1 = types.InlineKeyboardButton(text='Создать промокод', callback_data=f"promo")
+    adm.add(btn1)
+    adm.add(btn1, btn1)
+    bot.send_message(message.chat.id, message.from_user,reply_markup = adm )
 
 
 # обработака /help
 @bot.message_handler(commands=['help'])
 def start(message):
+    id = str(message.from_user.id)
     bot.send_message(message.chat.id,"""
 Столкнулись с проблемой?
 - Напишите в нашу службу поддержки: @lic_manager
@@ -77,7 +94,8 @@ def body(message):
 # обработка callback
 @bot.callback_query_handler(func=lambda c: True)
 def inline(c):
-    pass
+    if c.data == "promo":
+        bot.send_message(c.message.chat.id, c.message.chat.id)
 
 
 bot.skip_pending = True
